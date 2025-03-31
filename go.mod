677099f8bffe65f0b4b5f89f774a22dfb7b8280d
Divik Chotani <divik.chotani@gmail.com>
2025-03-30 23:39:20 -0700

    fixed bug in module, and added connection to port 6379

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main.go
+module redis-go
 
 go 1.24.1
--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	//"io"
+	"net"
+	//"os"
 )
 
 func main() {
-	fmt.Println("Hello World")
+
+	//stdout letting user know where server is running
+	fmt.Println("Listening on port 6379")
+
+	//create a new server
+
+	//create a listener object from net.listen that listens on port 6379
+	//specify a tcp connection
+	l, err := net.Listen("tcp", ":6379")
+
+	//error handling, print error and leave
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//create a connection from the listener
+	conn, err := l.Accept()
+	//error checking again
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//ensure closing connection at exit
+	defer conn.Close()
+
 }
